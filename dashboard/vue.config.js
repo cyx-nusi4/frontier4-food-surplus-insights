0e774fbb1bb13ce30ceb665f29057f2ce7ba6975
cyx-nusi4 <e1439450@u.nus.edu>
2025-06-11 09:31:14 +0800

    add nginx

--- dashboard/vue.config.js
+++ dashboard/vue.config.js
@@ -2,7 +2,8 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://127.0.0.1:5002',
+//                target: 'http://127.0.0.1:5002',
+                target: 'http://backend:5002',
                 changeOrigin: true,
                 pathRewrite: { '^/api': '' },
             },
